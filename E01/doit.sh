faf744347da659b434ac115fddd0b3cf0518fc83
Prashantha Kumanayake <pkumanayake@Prashanthas-MacBook-Air.local>
2025-04-17 17:39:01 +0300

    second program done

---
+++ E01/doit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Making a file"
+touch file.txt
+echo "Making a new directory"
+mkdir folder
+echo "Renameing file"
+mv file.txt textfile.txt
+echo "moving file"
+mv textfile.txt folder/textfilemoved.txtx
+
